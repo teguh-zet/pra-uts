4959f52bf0f401477775a1562981c387cff92474
teguh ahmadi zebua <zetteaz@gmail.com>
2024-06-30 23:41:04 +0700

    add lorem at HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,16 @@ const HomePage = () => {
     <div className="container mx-auto my-12 text-center">
       <h1 className="text-4xl font-bold mb-6">Selamat Datang di Aplikasi Buah-Buahan</h1>
       <p className="text-lg text-gray-700">
-        Ini adalah aplikasi untuk mengelola produk buah-buahan.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora optio praesentium adipisci error! Illo totam quam vitae illum voluptate hic rerum harum mollitia! Tempora aut quaerat odit. Dolores, maxime tempora.
+        Repellendus quasi sint magnam, vero quas expedita earum asperiores omnis et consequatur modi saepe error, placeat fuga, necessitatibus nam deserunt vitae illo inventore libero enim doloribus minima? Molestiae, blanditiis commodi.
+        Laboriosam aperiam repudiandae nemo. Esse, nemo cupiditate quis dolorem amet, dolore quaerat illo pariatur in voluptatem molestiae doloremque fugit, ipsum architecto deserunt commodi! Eum maxime quos hic eius est debitis!
+        Laboriosam explicabo ut tenetur, veniam iure expedita officia, quod delectus ducimus sed magnam consectetur non ex natus at. Iure nostrum harum repellat quidem voluptates hic a nihil odit labore similique?
+        Perspiciatis perferendis qui odit voluptates id incidunt et nobis ipsa facilis dicta animi magni accusantium provident at quidem aliquid fugit aspernatur enim tempora, consequatur sed minus ipsum. Eligendi, impedit error.
+        Omnis aliquid fugit sint consectetur, vel perferendis quis dicta quia impedit! Dolores nostrum eum a illum impedit aut illo quis exercitationem tempora voluptate, voluptatem ipsam soluta officiis, sequi, maxime molestias.
+        Maiores aliquam reprehenderit quibusdam quos, voluptates vero eligendi doloremque quaerat nihil nobis at ratione iure nam dolore culpa autem! Sit distinctio quis earum recusandae corrupti quisquam, impedit animi sapiente delectus?
+        Excepturi veniam voluptas recusandae rerum repellat ratione natus iusto, iure eius aliquid incidunt, maxime doloribus beatae necessitatibus saepe alias fugiat totam eaque sint. Architecto velit consectetur quis expedita! Sequi, possimus!
+        Laboriosam tempore possimus cumque facilis officia provident enim voluptas quod animi ab? Necessitatibus voluptatibus enim reiciendis possimus cupiditate, obcaecati nihil illum adipisci magni fuga cum eligendi blanditiis recusandae saepe nemo.
+        Provident quia harum ipsam eligendi, veniam et enim delectus, voluptatem, ducimus culpa doloribus aliquid sunt placeat quae quibusdam. Dicta voluptates exercitationem accusamus fugit quasi temporibus cupiditate vitae aliquid accusantium iste.
       </p>
     </div>
   );
